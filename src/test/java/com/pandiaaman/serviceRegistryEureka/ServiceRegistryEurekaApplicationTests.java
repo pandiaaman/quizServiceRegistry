4c769167054885c15e86c27c1b3b36314823850b
Aman Pandia <pandiaaman16@gmail.com>
2024-04-13 23:01:54 +0530

    first commit for service registry

---
+++ src/test/java/com/pandiaaman/serviceRegistryEureka/ServiceRegistryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandiaaman.serviceRegistryEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
